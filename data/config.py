6b5d01e05c96ad7d8f56869d165c297fcf99cfd6
Cai Hao <caihao.snake@gmail.com>
2020-01-21 00:06:21 -0500

    set up the twitter scraper to run.

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 """This file is the configuration for data directories and paths."""
 
-original_data_dir = '~/Dropbox/Insight_Jan2020'
+original_data_dir = '~/Dropbox/Insight_Jan2020/data'
 
 raw_data_dir = original_data_dir + '/raw'
 processed_data_dir = original_data_dir + '/processed'
--- scripts/scrape_tweets.py
+++ scripts/scrape_tweets.py
@@ -1,24 +1,28 @@
 """This script scrapes the tweets for companies and store them in csv files."""
+from data.config import raw_data_dir, processed_data_dir
 import pandas as pd
 import os
 
-raw_data_dir = '../data/raw'
-processed_data_dir = '../data/processed'
+processed_data_dir = '~/Dropbox/TEMP'
 
 company_twit_info = pd.read_csv(processed_data_dir + '/' + 'company_2014_twitter_info.csv')
-# print(company_twit_info)
 
 for i, row in company_twit_info.iterrows():
-
     twitter_username = row['twitter_username']
     twit_since = row['twit_since']
     twit_until = row['twit_until']
 
-    # sample twitterscraper query:
-    # twitterscraper "(from:Cyclica) until:2018-01-01 since:2017-01-01" -o Cyclica.csv -d
+    if not os.path.exists(processed_data_dir + '/tweets/{}.csv'.format(twitter_username)):
+        # sample twitterscraper query
+        # twitterscraper "(from:Cyclica) until:2018-01-01 since:2017-01-01" -d
 
-    print('twitterscraper "(from:{0}) until:{1} since:{2}" -o {3}/tweets/{0}.csv -c'\
-              .format(twitter_username, twit_until, twit_since, processed_data_dir))
-    # os.system('twitterscraper "(from:{0}) until:{1} since:{2}" -o {3}/tweets/{0}.csv -c'\
-    #           .format(twitter_username, twit_until, twit_since, processed_data_dir))
-    exit()
+        try:
+            # os.system('twitterscraper "(from:{0}) until:{1} since:{2}" -d ' \
+            # .format(twitter_username, twit_until, twit_since))
+            os.system('twitterscraper "(from:{0}) until:{1} since:{2}" -o {3}/tweets/{0}.csv -c' \
+                      .format(twitter_username, twit_until, twit_since, processed_data_dir))
+        except:
+            with open(processed_data_dir + '/tweets/error_companies.txt', 'a') as f:
+                f.write('{}, {}, {}\n'.format(twitter_username, twit_since, twit_until))
+
+    # exit()
