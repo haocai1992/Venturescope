54926090f6670b980bf5f50cb9df8b2366159859
Cai Hao <caihao.snake@gmail.com>
2020-01-20 22:54:18 -0500

    added data configuration file.

---
+++ data/config.py
@@ -0,0 +1,7 @@
+"""This file is the configuration for data directories and paths."""
+
+original_data_dir = '~/Dropbox/Insight_Jan2020'
+
+raw_data_dir = original_data_dir + '/raw'
+processed_data_dir = original_data_dir + '/processed'
+cleaned_data_dir = original_data_dir + '/cleaned'
