75610ebe3423f05d7dff1c09b68a1590c7d30a38
caihao <caihao.snake@gmail.com>
2020-01-22 17:43:02 -0500

    built the framework for the first prediction model.

---
+++ projectname/projectname/config.py
@@ -0,0 +1,11 @@
+
+classlabel2timelength = {
+    0: 'only series a',
+    1: '0 to 180 days',
+    2: '180 to 360 days',
+    3: '360 to 540 days',
+    4: '540 to 720 days',
+    5: '720 to 900 days',
+    6: '900 to 1080 days',
+    7: '1080 to 10000 days'
+}
---
+++ projectname/projectname/custom_funcs.py
@@ -0,0 +1,37 @@
+"""Functions needed to run the prediction models."""
+from data.config import home_dir, raw_data_dir, processed_data_dir, cleaned_data_dir
+from config import classlabel2timelength
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+class Predictor:
+    """A predictor to predict the time length for a company to raise another series of funding."""
+    def __init__(self, model_file, data_file):
+        self.model = self.load_model(model_file)
+        self.data = self.load_data(data_file)
+
+    @staticmethod
+    def load_model(model_file):
+        """Function to load a model."""
+        return pickle.load(open(model_file, 'rb'))
+
+    @staticmethod
+    def load_data(data_file):
+        """Function to load data."""
+        return pd.read_csv(data_file)
+
+    def predict(self, company_name):
+        """Predict the class of time length of a company in the training set."""
+        company_data = self.data[self.data.name == company_name].values[:, :-3]
+        predicted_len_class = self.model.predict(company_data)[0]
+        print(classlabel2timelength[predicted_len_class])
+
+
+
+def main():
+    p = Predictor(model_file=processed_data_dir + '/model_logreg1.pkl',
+                  data_file=processed_data_dir + '/model_logreg1_data.csv',)
+    for company in p.data.name:
+        p.predict(company)
+# main()
