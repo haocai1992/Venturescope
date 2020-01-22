e89cbbeaa3635dc4dd290376175e327993996d91
caihao <caihao.snake@gmail.com>
2020-01-22 17:40:42 -0500

    added home_dir for multi-device working convenience.

--- data/config.py
+++ data/config.py
@@ -1,8 +1,9 @@
 """This file is the configuration for data directories and paths."""
 
-original_data_dir = '~/Dropbox/Insight_Jan2020/data'
+home_dir = '/Users/caihao'
+original_data_dir = home_dir + '/Dropbox/Insight_Jan2020/data'
 
 raw_data_dir = original_data_dir + '/raw'
 processed_data_dir = original_data_dir + '/processed'
 cleaned_data_dir = original_data_dir + '/cleaned'
-tweets_data_dir = '/Users/caihao/Dropbox/TEMP/tweets'
+tweets_data_dir = home_dir + '/Dropbox/TEMP/tweets'
